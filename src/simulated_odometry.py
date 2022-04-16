e77f778b0dc4881b9770270bd971ff0cd9ab4ff9
Paweł Irzyk <pawel.irzyk6@gmail.com>
2022-04-16 20:03:21 +0200

    moved files

--- src/simulated_odometry.py
+++ src/simulated_odometry.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python3
 
+from gazebo_msgs.srv import GetModelState, GetModelStateRequest
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Header
-from gazebo_msgs.srv import GetModelState, GetModelStateRequest
 from tf import TransformBroadcaster
 import rospy
 import tf
@@ -12,10 +12,10 @@ Custom odometry publisher, based on the model state from gazebo
 This publisher is not used anywhere, exists only for veryfications and tests
 '''
 
-rospy.init_node('custom_odometry_node')
-odom_pub = rospy.Publisher('my_odom', Odometry)
-rospy.wait_for_service('/gazebo/get_model_state')       # wait for service
-get_model_srv = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)        #connect to service
+rospy.init_node('simulated_odometry')
+rospy.wait_for_service('/gazebo/get_model_state')
+get_model_srv = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+odom_pub = rospy.Publisher('my_odom', Odometry, queue_size=1)
 
 broadcaster = TransformBroadcaster()
 odom  = Odometry()
@@ -25,12 +25,10 @@ header.frame_id = 'odom'
 model = GetModelStateRequest()
 model.model_name = 'my_robot'
 
-r = rospy.Rate(5)
+rate = rospy.Rate(5)
 
 while not rospy.is_shutdown():
-    
     result = get_model_srv(model)
-    print(result)
     odom.pose.pose = result.pose
     odom.twist.twist = result.twist
 
@@ -38,8 +36,9 @@ while not rospy.is_shutdown():
     odom.header = header
 
     odom_pub.publish(odom)
-    broadcaster.sendTransform((result.pose.position.x, result.pose.position.y, 0), tf.transformations.quaternion_from_euler(0, 0, result.pose.orientation.z), rospy.Time.now(), 'base_footprint', 'odom')
-
-    r.sleep()
-
-
+    broadcaster.sendTransform((result.pose.position.x, result.pose.position.y, 0),
+            tf.transformations.quaternion_from_euler(0, 0, result.pose.orientation.z),
+            rospy.Time.now(),
+            'base_footprint',
+            'odom')
+    rate.sleep()
